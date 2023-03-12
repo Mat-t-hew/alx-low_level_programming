93d4a62c5793b1c6241afeeb63ec7ee8e722e034
Mat-t-hew <n.m.ikenna2@gmail.com>
2023-03-12 21:51:36 +0200

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+int _putchar(char c);
+
+#endif /*MAIN_H*/
+#ifndef MAIN_H_INCLUDED
+#define MAIN_H_INCLUDED
+
+int _putchar(char c);
+
+#endif /*MAIN_H*/
+
