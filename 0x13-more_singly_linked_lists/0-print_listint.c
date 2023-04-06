29f65c44961b7e797a541e328afeb8ec657233f3
Mat-t-hew <n.m.ikenna2@gmail.com>
2023-04-05 23:28:47 -0700

    0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,15 +8,17 @@
  */
 
 size_t print_listint(const listint_t *h);
+
 {
 	size_t x;
 
 	x = 0;
 
-	do{
+	do {
 		printf("%d\n", h->n);
 		x++;
-	}whil(h!=NULL);
-	return 0;
+		h = h->next;
+	} while (h != NULL);
+	return (x);
 }
 
