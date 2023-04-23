eead9771e5c66ef6b9077973b1e52bc1b6bf429a
Mat-t-hew <n.m.ikenna2@gmail.com>
2023-04-23 07:20:31 -0700

    2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,25 +17,25 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, bytes_written, length = 0;
 
-	if (file_name == NULL)
+	if (filename == NULL)
 	{
 		return (-1);
 	}
 
-	if (content != NULL)
+	if (text_content != NULL)
 	{
-		for (length = 0; content[length];)
+		for (length = 0; text_content[length];)
 		{
 			length++;
 		}
 	}
 
-	fd = open(file_name, O_WRONLY | O_APPEND);
-	bytes_written = write(fd, content, length);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	bytes_written = write(fd, text_content, length);
 
 	if (fd == -1 || bytes_written == -1)
 	{
-		return -1;
+		return (-1);
 	}
 
 	close(fd);
