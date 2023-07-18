a19f81c1bcad442a7c25faa50cc48b5d482f06d9
Mat-t-hew <n.m.ikenna2@gmail.com>
2023-07-18 08:56:14 -0700

    74

--- 0x15-file_io/100-elf_header.h
+++ 0x15-file_io/100-elf_header.h
@@ -1,44 +1,17 @@
-#ifndef ELF_HEADER_H
-#define ELF_HEADER_H
+#ifndef ELF_READER_H
+#define ELF_READER_H
 
-#include <stdint.h>
+#include <elf.h>
 
-#define ELF_HEADER_SIZE 64
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
 
-/**
- * Elf64_Ehdr - ELF header structure
- *
- * This structure represents the ELF header format for 64-bit architectures.
- * It contains various fields providing information about the ELF file.
- *
-
-typedef struct
-{
-	unsigned char e_ident[16];
-	uint16_t e_type;
-	uint16_t e_machine;
-	uint32_t e_version;
-	uint64_t e_entry;
-	uint64_t e_phoff;
-	uint64_t e_shoff;
-	uint32_t e_flags;
-	uint16_t e_ehsize;
-	uint16_t e_phentsize;
-	uint16_t e_phnum;
-	uint16_t e_shentsize;
-	uint16_t e_shnum;
-	uint16_t e_shstrndx;
-} Elf64_Ehdr;
-*/
-void print_error(const char *message);
-void print_magic(const unsigned char *e_ident);
-void print_class(const unsigned char *e_ident);
-void print_data(const unsigned char *e_ident);
-void print_version(const unsigned char *e_ident);
-void print_os_abi(const unsigned char *e_ident);
-void print_abi_version(const unsigned char *e_ident);
-void print_type(uint16_t e_type);
-void print_entry_point(uint64_t e_entry);
-void print_elf_header(const char *file_name);
-
-#endif /* ELF_HEADER_H */
+#endif /* ELF_READER_H */
